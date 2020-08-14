91749782b8f1fcf10c1dba7cf41e57d512878a47
mehdimerbah <mehdimerbah@outlook.com>
2020-08-14 10:04:02 +0100

    Solved the exercises.

--- Work/report.py
+++ Work/report.py
@@ -38,13 +38,6 @@ def read_portfolio_to_dict(filename):
     return portfolio
 
 
-my_stocks = read_portfolio_to_dict('Data/portfoliodate.csv')
-
-total_cost = 0.0
-for stock in my_stocks:
-    total_cost += stock['shares'] * stock['price']
-
-
 def read_prices(filename):
     prices = {}
     with open(filename, 'rt') as file:
@@ -58,16 +51,23 @@ def read_prices(filename):
     return prices
 
 
+my_stocks = read_portfolio_to_dict('Data/portfoliodate.csv')
 stock_prices = read_prices('Data/prices.csv')
 
-total_value = 0
-for stock in my_stocks:
-    total_value += stock['shares'] * stock_prices[stock['name']]
+
+def compute_stock_summary():
+    total_cost = 0.0
+    for stock in my_stocks:
+        total_cost += stock['shares'] * stock['price']
+
+    total_value = 0
+    for stock in my_stocks:
+        total_value += stock['shares'] * stock_prices[stock['name']]
+
+    return total_cost, total_value
 
 
-# print(f'Total Cost: {total_cost: .2f}')
-# print(f'Current Value: {total_value: .2f}')
-# print(f'Gain: {(total_value - total_cost): .2f}')
+portfolio_cost, portfolio_value = compute_stock_summary()
 
 
 def make_report(portfolio, prices):
@@ -84,29 +84,13 @@ def make_report(portfolio, prices):
 
 report = make_report(my_stocks, stock_prices)
 
-headers = ('Name', 'Shares', 'Price', 'Change')
-print('%10s %10s %10s %10s' % headers)
 
-print(('-'*10 + ' ')*4)
-
-for entry in report:
-    print(f'{entry[0]: >10s} {entry[1]: >10d} {entry[2]: >10.2f} {entry[3]:>10.2f}')
-
-
+def print_report(report):
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print('%10s %10s %10s %10s' % headers)
+    print(('-' * 10 + ' ') * 4)
+    for entry in report:
+        print(f'{entry[0]: >10s} {entry[1]: >10d} {entry[2]: >10.2f} {entry[3]:>10.2f}')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print_report(report)
